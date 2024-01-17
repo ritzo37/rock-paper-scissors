e40955ccd3bb47c7053dc5b9e9f4ef896ecc5925
ritzo37 <ritishrai56@gmail.com>
2024-01-18 02:00:46 +0530

    Add calcResult() function to script.js

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ function getUserChoice() {
     let choice = prompt("Enter your choice !");
     return choice;
 }
+function calcResult(userChoice,computerChoice) {
+    if (userChoice===computerChoice) {
+      return "Draw" ; 
+    }
+    else if (userChoice=="ROCK") {
+      if (computerChoice=="PAPER") {
+          return "LOSS"
+      }
+      else { // here the computerchoice will be Scissor 
+          return "WIN"; 
+      }
+    }
+    else if (userChoice=="PAPER") {
+      if (computerChoice=="SCISSOR") {
+          return "LOSS" ;
+      }
+      else { // here the computerchoice will be Rock
+          return "WIN" ; 
+      }
+    }
+    else { // here the userChoice will be Scissor
+      if (computerChoice=="PAPER") {
+          return "WIN" ;
+      }
+      else { // here the computerChoice will be rock 
+          return "LOSS" ; 
+      }
+    }
+}
